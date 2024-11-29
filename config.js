91357904f68bb15f7c833c423e2403ccb0e58211
Vishwa500 <143321672+Vishwa500@users.noreply.github.com>
2024-11-29 17:57:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "zz5XQB5S#eMfgkKYeqZzoJnUZhpbtqnRsoJUA0Cjc1QUiNCCt4sM", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "privet",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
